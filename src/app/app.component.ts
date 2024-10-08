e7fdf7fa7ed10c247f82a20fb993cb8a777c5dbd
Neposyda <v.guard333@gmail.com>
2024-10-08 13:43:24 +0300

    1 Edit hero / two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./components/heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeroesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
