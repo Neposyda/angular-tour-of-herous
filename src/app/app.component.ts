687b9ddc3731332fdaa601732d8168fb194ec007
Neposyda <v.guard333@gmail.com>
2024-10-08 14:38:34 +0300

    1 Edit hero / app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from "./components/heroes/heroes.component";
+// import { RouterOutlet } from '@angular/router';
+// import { HeroesComponent } from "./components/heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeroesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
