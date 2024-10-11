9dfafb062b0ebb279541ebc76e31b39c6a6dbcfd
Neposyda <v.guard333@gmail.com>
2024-10-11 13:13:16 +0300

    6 Get data from a server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { HeroesComponent } from "./components/heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
